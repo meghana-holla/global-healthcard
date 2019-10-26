16d0fb41256f7af2708d5bf4ca77d4098644f616
Naklecha <nishant.aklecha@gmail.com>
2019-10-26 13:40:07 +0530

    Register Patient correction

--- backend.py
+++ backend.py
@@ -22,14 +22,13 @@ api = Api(app)
 class register_patient(Resource):
     def post(self):
         req = eval(request.data.decode())
-        if(req):
-            public_key = req["public"]
-            private_key = req["private"]
-            transaction  = contract.functions.initpat(req["name"],req["age"],req["blood_group"]).buildTransaction()
-            transaction['nonce'] = web3.eth.getTransactionCount(public_key)
-            signed_tx = web3.eth.account.signTransaction(transaction, private_key)
-            tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-            return str(tx_hash),200
+        public_key = req["public"]
+        private_key = req["private"]
+        transaction  = contract.functions.initpat(req["name"],req["age"],req["blood_group"]).buildTransaction()
+        transaction['nonce'] = web3.eth.getTransactionCount(public_key)
+        signed_tx = web3.eth.account.signTransaction(transaction, private_key)
+        tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return str(tx_hash),200
 
 api.add_resource(register_patient, '/register_patient')
 
